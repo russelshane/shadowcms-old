ca8e31ee12147d6cb7d33254229d4171d0c662e5
Darsler Russel Shane <darsler@pm.me>
2021-08-10 12:24:03 +0800

    (staging): change bylines type to array of strings

--- api/source/interfaces/header-interface.ts
+++ api/source/interfaces/header-interface.ts
@@ -3,12 +3,10 @@
   ShadowCMS
 */
 
-import { UserModel as Byline } from "../models/user-model";
-
 export interface ArticleHeader {
   headline?: string;
   header_type?: string;
   summary?: string;
   label?: string;
-  bylines?: Byline;
+  bylines?: string[];
 }
