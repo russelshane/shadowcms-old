f93c85cf24641f8fd2f7a07184b31d9056549807
russelshane <darsler@pm.me>
2021-09-09 14:32:32 +0800

    (staging): add source directory

---
+++ client2/src/application.tsx
@@ -0,0 +1,16 @@
+/**
+ * @description Main Application Component
+ * @author ShadowCMS
+ */
+
+import React from "react";
+
+const Application: React.FC = () => {
+  return (
+    <div>
+      <h1>hey</h1>
+    </div>
+  );
+};
+
+export default Application;
---
+++ client2/src/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * @description Shadow
+ * @author ShadowCMS
+ */
+
+import ReactDOM from "react-dom";
+import Application from "./application";
+
+/**
+ * @description Render main application to react-root div element
+ */
+ReactDOM.render(<Application />, document.getElementById("react-root"));
