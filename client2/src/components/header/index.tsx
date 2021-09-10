ae8563a140a4d4b18ddd363d8b6f30f50526add0
russelshane <darsler@pm.me>
2021-09-10 14:01:30 +0800

    (staging): move all mocks to one directory

--- client2/src/components/header/index.tsx
+++ client2/src/components/header/index.tsx
@@ -9,6 +9,7 @@ import HeaderProps from "./types";
 import mockUser from "../../mocks/user";
 import { HeaderContainer } from "./style";
 import { BellIcon, MenuIcon } from "@heroicons/react/outline";
+import mockNotifications from "../../mocks/notifications";
 
 /* Dynamic Components */
 const Avatar = loadable(() => import("../../ui/avatar"));
@@ -19,23 +20,6 @@ const Dropdown = loadable(() => import("../../ui/dropdown"));
 
 const Header: React.FC<HeaderProps> = () => {
   const [notificationsActive, setNotificationsActive] = useState(false);
-
-  const mockNotifs = [
-    {
-      label: "Attention! An article under your byline needs correction",
-      path: "/n/3409rgeklj",
-      description:
-        "An article that appears you wrote is in need of some corrections, please take action as soon as possible.",
-      date: `2021-09-10T13:29:41`,
-    },
-    {
-      label: "John Doe published your article",
-      path: "/n/ae2234903",
-      description:
-        "Your article 'Biden Mandates Vaccines for Workers, Saying, ‘Our Patience Is Wearing Thin’' has been published and is now live.",
-      date: `2021-09-10T13:29:22`,
-    },
-  ];
 
   return (
     <HeaderContainer>
@@ -48,7 +32,7 @@ const Header: React.FC<HeaderProps> = () => {
           <Button icon={<BellIcon />} onClick={() => setNotificationsActive(!notificationsActive)}>
             Notifications
           </Button>
-          <Dropdown items={mockNotifs} active={notificationsActive} />
+          <Dropdown items={mockNotifications} active={notificationsActive} />
         </Container>
         <Avatar withMenu={true} user={mockUser} />
       </Container>
---
+++ client2/src/mocks/notifications.ts
@@ -0,0 +1,18 @@
+const mockNotifications = [
+  {
+    label: "Attention! An article under your byline needs correction",
+    path: "/n/3409rgeklj",
+    description:
+      "An article that appears you wrote is in need of some corrections, please take action as soon as possible.",
+    date: `2021-09-10T13:29:41`,
+  },
+  {
+    label: "John Doe published your article",
+    path: "/n/ae2234903",
+    description:
+      "Your article 'Biden Mandates Vaccines for Workers, Saying, ‘Our Patience Is Wearing Thin’' has been published and is now live.",
+    date: `2021-09-10T13:29:22`,
+  },
+];
+
+export default mockNotifications;
